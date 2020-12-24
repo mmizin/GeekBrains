6c38ac22c88f34ce26cfe0a62812db8bdac84053
mmizin <mmizin@atlassian.com>
2020-12-24 11:30:29 +0200

    python_essential_lesson_1:Finish home work

---
+++ python_essential/lesson_1/ex_1.py
@@ -0,0 +1,9 @@
+def main():
+    name = input('Enter your name ')
+    age = int(input('Enter your age '))
+
+    print(f'Hi {name}, nice to meet you! Your age is {age}')
+
+
+if __name__ == '__main__':
+    main()
---
+++ python_essential/lesson_1/ex_3.py
@@ -0,0 +1,8 @@
+def main():
+    num = input('Enter your number: ')
+
+    print(int(num) + int(num*2) + int(num*3))
+
+
+if __name__ == '__main__':
+    main()

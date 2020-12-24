b77843598032754d126e8567863d1e12978d7e4b
mmizin <mmizin@atlassian.com>
2020-12-24 12:10:36 +0200

    home_work_1:Update ex_6 script

--- python_essential/lesson_1/ex_6.py
+++ python_essential/lesson_1/ex_6.py
@@ -1,9 +1,14 @@
-start = int(input("Start from: "))
-expected_result = int(input("Start from: "))
-day = 1
+def main():
+    start = int(input("Start from: "))
+    expected_result = int(input("Start from: "))
+    day = 1
 
-while start < expected_result:
-    start += start / 10
-    day += 1
+    while start < expected_result:
+        start += start / 10
+        day += 1
 
-print(f'On the {day} day our sportsman got expected result')
+    print(f'On the {day} day our sportsman will get expected result.')
+
+
+if __name__ == '__main__':
+    main()

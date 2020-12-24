fd596975d9d80f665bdf60805851aa128f4d848c
mmizin <mmizin@atlassian.com>
2020-12-24 11:33:31 +0200

    Revert "python_essential_lesson_1:Finish home work"
    This reverts commit 6c38ac22c88f34ce26cfe0a62812db8bdac84053.

--- python_essential/lesson_1/ex_1.py
+++
@@ -1,9 +0,0 @@
-def main():
-    name = input('Enter your name ')
-    age = int(input('Enter your age '))
-
-    print(f'Hi {name}, nice to meet you! Your age is {age}')
-
-
-if __name__ == '__main__':
-    main()
--- python_essential/lesson_1/ex_3.py
+++
@@ -1,8 +0,0 @@
-def main():
-    num = input('Enter your number: ')
-
-    print(int(num) + int(num*2) + int(num*3))
-
-
-if __name__ == '__main__':
-    main()

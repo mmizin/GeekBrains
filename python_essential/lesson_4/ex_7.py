e6c4d6ce73d1ffe088e00a55252b7642be9084d8
mmizin <mmizin@atlassian.com>
2021-01-13 19:21:59 +0200

    home_work_4:Finish home work 4

---
+++ python_essential/lesson_4/ex_7.py
@@ -0,0 +1,30 @@
+# Реализовать генератор с помощью функции с ключевым словом yield, создающим очередное значение.
+# При вызове функции должен создаваться объект-генератор.
+# Функция должна вызываться следующим образом: for el in fact(n).
+# Функция отвечает за получение факториала числа, а в цикле необходимо выводить только первые n чисел, начиная с 1! и до n!.
+# Подсказка: факториал числа n — произведение чисел от 1 до n. Например, факториал четырёх 4! = 1 * 2 * 3 * 4 = 24.
+
+def infinite_sequence(num):
+    count = 1
+    factorial = 1
+    while num:
+        factorial *= count
+        yield factorial
+        count += 1
+        num -= 1
+
+
+result_string = ''
+
+for index, fact in enumerate(infinite_sequence(4), 1):
+    if index == 4:
+        result_string += f'{index} = {fact}'
+        break
+    result_string += f'{index} * '
+
+print(result_string)
+
+
+
+
+
